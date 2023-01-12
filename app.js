ec130429a4d1fa4324a0d2dbdc007eeb2d9663ed
mailsondias <mailsondg@gmail.com>
2023-01-12 16:47:30 -0300

    chore: Improvements to unit test

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 /**
- * Letter app
+ * Letter app v2
  */
 
 const api_users = 'https://jsonplaceholder.typicode.com/users'
@@ -7,32 +7,34 @@ const api_posts = 'https://jsonplaceholder.typicode.com/posts'
 
 const app = document.querySelector('#app')
 
-let users = []
+const Letter = {
+    get: async () => {
+        try {
+            const users = await getUsers()
 
-async function Letter() {
-    users = await getUsers()
+            for (let i = 0; i < users.length; i++) {
+                const arrPosts = await getUserPosts(users[i].id)
+                users[i].posts = [...arrPosts]
+            }
+
+            return users
+        } catch (err) {
+            console.error(err)
+        }
+    }
+}
+
+async function onMounted() {
+    const letterUsers = await Letter.get()
+    app.innerHTML = `<pre>${JSON.stringify(letterUsers, null, 2)}</pre><br />`
 }
 
 async function getUsers() {
-    fetch(api_users)
-        .then(res => res.json())
-        .then(arrUsers => {
-            arrUsers.forEach(user => {
-
-                fetch(api_users + '/' + user.id + '/posts')
-                    .then(res2 => res2.json())
-                    .then(arrPosts => {
-                        user.posts = arrPosts
-
-                        app.innerHTML = app.innerHTML + `<pre>${JSON.stringify(user, null, 2)}</pre><br>`
-                    })
-                    .catch(err => console.log(err))
-
-            })
-
-            return arrUsers
-        })
-        .catch(err => console.log(err))
+    const res = await fetch(api_users)
+    return await res.json()
 }
 
-Letter()
+async function getUserPosts(userId) {
+    const res = await fetch(`${api_users}/${userId}/posts`)
+    return await res.json()
+}
